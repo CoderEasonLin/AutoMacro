58fc6337fc5d38e6ff7789f6db808281e12f2963
Eason Lin <lin07646@gmail.com>
2016-04-17 16:30:19 +0800

    1. Get window handle. 2. Get window position. 3. Get mouse position of this window. 4. Send message to this position of this window.

--- AutoMacro/Form1.Designer.cs
+++ AutoMacro/Form1.Designer.cs
@@ -1,11 +1,14 @@
-﻿namespace AutoMacro
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace AutoMacro
 {
     partial class Form1
     {
         /// <summary>
         /// Required designer variable.
         /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private IContainer components = null;
 
         /// <summary>
         /// Clean up any resources being used.
@@ -28,12 +31,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 41);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 209);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Button button2;
     }
 }
 
--- AutoMacro/Form1.cs
+++ AutoMacro/Form1.cs
@@ -1,20 +1,158 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AutoMacro
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ClientToScreen(IntPtr hWnd, ref Point lpPoint);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool ScreenToClient(IntPtr hWnd, ref Point lpPoint);
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SendMessage(IntPtr hWnd, int message, int wParam, int lParam);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;        // x position of upper-left corner  
+            public int Top;         // y position of upper-left corner  
+            public int Right;       // x position of lower-right corner  
+            public int Bottom;      // y position of lower-right corner  
+        }
+
+        enum KeyModifier
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            WinKey = 8
+        }
+
+        class TargetApplication
+        {
+            public string Title { get; set; }
+
+            public int SizeX
+            {
+                get { return Rect.Right - Rect.Left; }
+            }
+
+            public int SizeY
+            {
+                get { return Rect.Bottom - Rect.Top; }
+            }
+
+            public IntPtr Handle { get; set; }
+            public RECT Rect { get; set; }
+        }
+
+        private TargetApplication target = new TargetApplication();
+
+        protected override void WndProc(ref Message message)
+        {
+            base.WndProc(ref message);
+
+            if (message.Msg == 0x0312)
+            {
+                Keys key = (Keys)(((int)message.LParam >> 16) & 0xFFFF);                  // The key of the hotkey that was pressed.
+                KeyModifier modifier = (KeyModifier)((int)message.LParam & 0xFFFF);       // The modifier of the hotkey that was pressed.
+                int id = message.WParam.ToInt32();                                        // The id of the hotkey that was pressed.
+
+                GetCurrentProcess();
+            }
+        }
+
         public Form1()
         {
             InitializeComponent();
+
+            int id = 100;
+            RegisterHotKey(Handle, id, (int)KeyModifier.Shift, Keys.A.GetHashCode());
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            int id = 100;
+            UnregisterHotKey(Handle, id);
+        }
+
+        private void GetCurrentProcess()
+        {
+            
+            var handle = GetForegroundWindow();
+            int count = 255;
+            var stringBuilder = new StringBuilder();
+            GetWindowText(handle, stringBuilder, count);
+
+            RECT rect;
+            GetClientRect(handle, out rect);
+            var mousePosition = MousePosition;
+            var sb = new StringBuilder();
+
+            target.Handle = handle;
+            target.Title = stringBuilder.ToString();
+            target.Rect = rect;
+
+            ScreenToClient(target.Handle, ref mousePosition);
+            position = mousePosition;
+
+            sb.AppendFormat("Window Title: {0}\n", stringBuilder);
+            sb.AppendFormat("Window Position: Top {0} Bottom {1} Left {2} Right {3}\n", rect.Top, rect.Bottom, rect.Left, rect.Right);
+            sb.AppendFormat("Window Size: X {0} Y {1}\n", target.SizeX, target.SizeY);
+            sb.AppendFormat("Current Mouse Position: X {0} Y {1}\n", mousePosition.X, mousePosition.Y);
+            sb.AppendFormat("Current Mouse Position of window: X {0} Y {1}\n", mousePosition.X - rect.Left,
+                mousePosition.Y - rect.Top);
+            
+
+            richTextBox1.Text = sb.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var proc = Process.GetCurrentProcess();
+
+        }
+
+
+        private Point position;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = position.X;
+            int y = position.Y;
+            int pos = ((y << 0x10) | x);
+            SendMessage(target.Handle, (int)0x201, 1, pos);
+            SendMessage(target.Handle, (int)0x202, 0, pos);
         }
     }
 }
